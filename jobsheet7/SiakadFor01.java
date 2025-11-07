07ba3c7b9c81aa16991f23e038c71c850fd163d6
adeliaamantaa <adelputri463@gmail.com>
2025-11-07 10:58:15 +0700

    Menambahkan fitur perhitungan mahasiswa lulus dan tidak lulus menggunakan perulangan for

--- jobsheet7/SiakadFor01.java
+++ jobsheet7/SiakadFor01.java
@@ -6,6 +6,9 @@ public class SiakadFor01 {
         Scanner sc = new Scanner(System.in);
         
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+
+        System.out.println("===== PROGRAM PENILAIAN MAHASISWA =====");
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -16,11 +19,22 @@ public class SiakadFor01 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
         System.out.println("\n==============================");
         System.out.println("Nilai tertinggi : " + tertinggi);
         System.out.println("Nilai terendah  : " + terendah);
+        System.out.println("------------------------------");
+        System.out.println("Jumlah mahasiswa lulus        : " + lulus);
+        System.out.println("(mahasiswa dengan nilai >= 60)");
+        System.out.println("Jumlah mahasiswa tidak lulus  : " + tidakLulus);
+        System.out.println("(mahasiswa dengan nilai < 60)");
         System.out.println("==============================");
 
         sc.close();
